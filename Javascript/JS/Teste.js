c037e06ac9863568e5792cd777432cc3d956fc21
WylkerVictor <149328870+WylkerVictor@users.noreply.github.com>
2024-07-25 11:34:25 -0300

    Delete Javascript/JS directory

--- Javascript/JS/Teste.js
+++
@@ -1,6 +0,0 @@
-let readline = require('readline-sync');
-
-let metros = readline.question('Digite metros você quer converter para centimentros: ');
-console.log(metros);
-resultado = metros * 100;
-console.log(`Valor digitado: ${metros}m | Resultado: ${resultado}cm`);
--- Javascript/JS/Teste2.js
+++
@@ -1,7 +0,0 @@
-const readline = require('readline-sync');
-
-const dinheiro = readline.question('Digite quanto voce ganha por hora: ');
-const tempo = readline.question('Digite quantas horas voce trabalha por mes: ');
-let total;
-total = dinheiro * tempo;
-console.log(`Salário: R$ ${total}`);
